d2e00f6704462d3de67ce8c8487d17fb6a33bb0e
hyunju1211 <hyunju0411@konkuk.ac.kr>
2025-05-19 21:02:30 +0900

    [DESIGN!] TailwindCSS 도입

--- ccd-renderer/src/App.jsx
+++ ccd-renderer/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div className="bg-red-500 text-white p-10 rounded-xl">
+  TailwindCSS 적용 확인용 블록입니다~람쥐
+</div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -17,13 +21,16 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="text-3xl font-bold text-blue-600">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          checkche dckcheck {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          왜 안바뀌죠??? <code>src/App.jsx</code> and save to test HMR
         </p>
+            <div className="p-4 bg-blue-500 text-white rounded-xl">
+            Hello Tailwind!
+          </div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
