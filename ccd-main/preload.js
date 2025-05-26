6a1abf88f936f04ba7458cfade2c3722402cb945
soobin <ihappy0331@konkuk.ac.kr>
2025-05-26 17:38:35 +0900

    [UPDATE] main.js 수정

--- ccd-main/preload.js
+++ ccd-main/preload.js
@@ -1,3 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
   console.log('Preload loaded.');
 });
+
+// preload.js
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  closeWindow: () => ipcRenderer.send("close-window"),
+});
