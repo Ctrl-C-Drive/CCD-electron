3c36d4d10dd4a00dfaae4466a75bd62004c83b72
rayeonghur <140002805+rayeonghur@users.noreply.github.com>
2025-06-04 13:46:34 +0900

    [FIX] DataLoad 호출 함수 수정
    merge까지 데이터레포지토리에 되어있는 함수 있음.

--- ccd-main/Dataload.js
+++ ccd-main/Dataload.js
@@ -4,14 +4,7 @@ const dataRepo = require("./db_models/DataRepository");
 
 ipcMain.handle("load-clipboard-records", async (_, isLogin) => {
   try {
-    const localData = await dataRepo.getLocalPreview();
-    let cloudData = [];
-
-    if (isLogin) {
-      cloudData = await dataRepo.getCloudPreview();
-    }
-
-    const merged = dataRepo.mergeItems(localData, cloudData);
+    const merged = dataRepo.getPreviewData();
     return { success: true, data: merged };
   } catch (err) {
     console.error("기록 보기 로딩 오류:", err);
