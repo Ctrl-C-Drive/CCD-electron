7b15b9d4111e7dbc4864aceee9932fe4c233da2e
Ptldms <ptldms56@gmail.com>
2025-06-09 01:56:50 +0900

    [FIX] 붙여넣기를 위한 복사 데이터가 기록창에 표시되지 않도록 수정

--- ccd-main/paste.js
+++ ccd-main/paste.js
@@ -5,6 +5,7 @@ const https = require("https");
 const { Buffer } = require("buffer");
 const DataRepository = require("./db_models/DataRepository"); // 경로는 실제 위치에 맞게 수정
 const CCDError = require("./CCDError");
+const monitor = require("./clipboard/monitor");
 
 /**
  * 이미지 URL을 읽어 클립보드에 복사
@@ -44,12 +45,19 @@ async function pasteById(dataId) {
 
     if (type === "text") {
       clipboard.writeText(item.content);
+      monitor.skipNextCopy(item.content, "text"); // 예외 등록
     } else if (type === "image") {
       if (item.shared === "cloud" && item.content.startsWith("http")) {
         await copyImageFromUrl(item.content);
+        // URL로 복사된 이미지는 해시값을 구해 예외 등록
+        const image = clipboard.readImage();
+        const hash = require("crypto").createHash("sha256").update(image.toPNG()).digest("hex");
+        monitor.skipNextCopy(hash, "image");
       } else {
         const image = nativeImage.createFromPath(item.content);
         clipboard.writeImage(image);
+        const hash = require("crypto").createHash("sha256").update(image.toPNG()).digest("hex");
+        monitor.skipNextCopy(hash, "image");
       }
     } else {
       throw new Error(`지원하지 않는 타입: ${type}`);
